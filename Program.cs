ca0b6af02972b37b759a525327d7fe1e746956f5
John Carlo Nayan <nayanjohncarlo123@gmail.com>
2026-02-21 16:51:21 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program {
+    static void Main()
+    {
+        Console.WriteLine("PAYMENT METHODS");
+
+        Console.WriteLine("Select a payment method (1-4): ");
+        Console.WriteLine("1. Cash" + " 2. Online Payment" + " 3. Credit Card" + " 4. Add a Bank Account");
+        string paymentMethod = Console.ReadLine();
+
+        if (paymentMethod == "Cash")
+        {
+            Console.WriteLine("You have selected Cash.");
+            Console.WriteLine("The total amount is:" + " " + "Please pay the amount upon delivery. Thank you!");
+
+        }
+
+
+
+
+
+
+    }
+}
